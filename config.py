d858e039f9f5595a7a749327caf490ed7714411d
Nirmal raj <128925724+nirmalraj002@users.noreply.github.com>
2024-08-03 23:43:57 +0530

    ..

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ load_dotenv()
 API_ID = int(getenv("API_ID", "29174394"))
 API_HASH = getenv("API_HASH", "2c83df6681819061fb6951c8d482b661")
 
-BOT_TOKEN = getenv("BOT_TOKEN", "7254375706:AAF_ZivIzvn8Kzn2JshsktiRIU-1c5-ay90")
+BOT_TOKEN = getenv("BOT_TOKEN", "7254375706:AAE-6wjyk5d3Ja0B16Wcs3rkgZQisxZfG_E")
 MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://vasudurairam:vasudurai123@cluster01.3l3sszt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster01")
 
 OWNER_ID = int(getenv("OWNER_ID", 6916055129))
