0a246cf21bca2109e8d573142503db3e0e6136c1
Nirmal raj <128925724+nirmalraj002@users.noreply.github.com>
2024-08-03 23:37:58 +0530

    ..

--- config.py
+++ config.py
@@ -12,5 +12,5 @@ BOT_TOKEN = getenv("BOT_TOKEN", "7254375706:AAF_ZivIzvn8Kzn2JshsktiRIU-1c5-ay90"
 MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://vasudurairam:vasudurai123@cluster01.3l3sszt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster01")
 
 OWNER_ID = int(getenv("OWNER_ID", 6916055129))
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/FallenAssociation")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/universee_we_are")
 START_IMG_URL = getenv("START_IMG_URL", "https://telegra.ph/file/6b8905e7e063a96bc6030.jpg")
