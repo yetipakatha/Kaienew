7c9df4285692ff8a9e1e555e926a6d2b422f5a0c
Nirmal raj <128925724+nirmalraj002@users.noreply.github.com>
2024-08-03 23:47:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,5 +12,5 @@ BOT_TOKEN = getenv("BOT_TOKEN", "7254375706:AAE-6wjyk5d3Ja0B16Wcs3rkgZQisxZfG_E"
 MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://vasudurairam:vasudurai123@cluster01.3l3sszt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster01")
 
 OWNER_ID = int(getenv("OWNER_ID", 6916055129))
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/universee_we_are")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/we_are_universee")
 START_IMG_URL = getenv("START_IMG_URL", "https://telegra.ph/file/7f02fa86d8b5f924be21b.jpg")
